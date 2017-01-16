c74b5cf7c9fe6084b42d679b43ac5effd6ddc1d4
David Gulyas <gulyman@gmail.com>
2017-01-16 00:09:57 -0700

    Add CPU H, and CPU G program
    Finish the program for CPU G
    Create CPU H in pres2

--- progs/G_multInput.asm
+++ progs/G_multInput.asm
@@ -1,10 +1,10 @@
 lk 0			#load the first character x
 lk 1			#load the second character y
-minus 0 6 0	48	#convert from ascii to number
+minus 0 6 0	48	#convert from ascii to number (minus 48)
 minus 1 6 1	48	#convert from ascii to number
-pt 6 2 0		#loop counter
-pt 6 3 0		#working total
+pt 6 2 0		#loop counter (load 0)
+pt 6 3 0		#working total (load 0)
 add 0 3 3		#add x to working total (top of loop)
 add 6 2 2 1		#increment loop counter by 1
 jlt 2 1 6 7 6	#if loop counter < x then jump back to the top of loop
-add 6 3 6 48	#Add 48 to working total and pass to screen
+add 6 3 6 48	#Convert working total to ascii (add 48) and pass to screen
